d7240ceef304d95c9dd857ae1ae34b6860257df5
thenoodlewarrior <nmmenrath@gmail.com>
2022-03-27 18:59:42 -0400

    Added container div id as parent for square div class

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 const body = document.querySelector('body');
 
+const div = document.createElement('div');
+const container = document.createElement('div');
+
+container.setAttribute('id', 'container');
+
+body.append(container);
 for (i=0; i < 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
-    body.append(div);
+    container.append(div);
 }
