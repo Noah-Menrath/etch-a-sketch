0c4da2ad5a98800859cdc7d46106f2943d425e5d
thenoodlewarrior <nmmenrath@gmail.com>
2022-03-27 23:33:40 -0400

    working on mouseover effect of squares

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
 const body = document.querySelector('body');
 
-const div = document.createElement('div');
+
 const container = document.createElement('div');
 
 container.setAttribute('id', 'container');
 
+o = (16 * 16); // have an input box for how many boxes user wants or even have a few button choices //
+
 body.append(container);
-for (i=0; i < 256; i++) {
+for (i=0; i < o; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
     container.append(div);
-}
+    div.addEventListener("mouseover", function() {
+        document.getElementsByClassName("square")[5].style.backgroundColor = "black";
+    })
+
+
+    
+};
+
+
+// const div = document.getElementsByClassName("square");
+
+// function runGame() {
+    
+// runGame()
+
+// div.addEventListener("mouseover", changeColor);
